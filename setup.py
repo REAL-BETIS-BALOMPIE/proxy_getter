a913318e4f4ef9d62904bedc1dd4758cc07da8be
dander94 <3balejandro@gmail.com>
2020-10-15 12:36:21 +0200

    Add License, Manifest and Setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proxy-getter-dander94",
+    version="0.1.0",
+    author="dander94",
+    author_email="",
+    description="Simple HTTPS Proxy Getter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/dander94/proxy-getter",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
