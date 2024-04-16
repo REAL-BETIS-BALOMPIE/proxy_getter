23eebd401bd086d0985bb2a8d2478022e64713f9
adiazrbb <adiaz@realbetisbalompie.es>
2024-04-16 12:28:21 +0200

    chore: remove textProxy.py

--- proxy_getter/testProxy.py
+++
@@ -1,7 +0,0 @@
-from proxy_getter import get_proxy, check_proxy
-
-TM_DOMAIN = 'https://www.transfermarkt.es'
-
-proxy = None
-proxy = get_proxy(discard_proxy=proxy, check_against=TM_DOMAIN)
-print("Proxy Encontrado: " + proxy)
